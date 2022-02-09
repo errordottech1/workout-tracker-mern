7b70a07b5bf8ecf0f8450d0f114df8e9b5ec95a6
Husein Rezaei Fard <errordottech@gmail.com>
2022-02-09 14:40:06 +0330

    debug the server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import exerciseRoutes from "./routes/exerciseRoutes.js";
-import { handler } from "./middleware/error.js";
 
 dotenv.config();
 
@@ -12,8 +11,6 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
 server.use("/api/exercise", exerciseRoutes);
-
-server.use(handler);
 
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
